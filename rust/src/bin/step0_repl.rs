dff20bae8b89b8098aa420ac30e07258f90bbd85
solomon-the-wise <99106363+solomon-the-wise@users.noreply.github.com>
2022-12-20 10:35:45 -0500

    finished step 0

--- rust/src/bin/step0_repl.rs
+++ rust/src/bin/step0_repl.rs
@@ -1,5 +1,34 @@
 extern crate mal_rust;
 
-fn main() {
+use std::io;
+use std::io::{stdout, Write};
+
+fn read(text: String) -> String{
+    return text
+}
+fn eval(text: String) -> String{
+    return text
 }
 
+fn print(text: String) -> String {
+    return text
+}
+
+fn rep(text: String) -> String {
+    let text= read(text);
+    let text= eval(text);
+    let text = print(text);
+    return text
+}
+fn main() {
+
+    let standard_input = io::stdin();
+    while true {
+        let mut buffer = String::new();
+        print!("user> ");
+        stdout().flush();
+        standard_input.read_line(&mut buffer);
+        let result = rep(buffer);
+        print!("{}", result)
+    }
+}
